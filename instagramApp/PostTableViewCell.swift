6cc74b2905c7b1bb189c658384bfc557e8c1c75e
Kent Canonigo <kcanonigo@middlebury.edu>
2022-10-14 23:24:13 -0400

    User can query posts

---
+++ instagramApp/PostTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  PostTableViewCell.swift
+//  instagramApp
+//
+//  Created by Kent Brylle Canonigo on 10/14/22.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
